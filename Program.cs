368293902aea06e7412eaac81b8201392b27506f
bastardcode <63298879+bastardcode@users.noreply.github.com>
2020-04-14 15:52:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace MyDistanceMove
 {
@@ -9,15 +11,16 @@ namespace MyDistanceMove
             MoveAndSpreed moveAndSpreed = new MoveAndSpreed();
 
             const string Sharp = "######################################################################################"; // ### lines.
+            const string Log = "Log: ";
 
-            Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine($"FAQ... hours, {Environment.NewLine} ## 1 - 10 minutes, 2 - 20 minutes, 3 - 30 minutes. {Environment.NewLine} ## 4 - 40 minutes, 5 - 50 minutes, 6 - 60 minutes. {Environment.NewLine} ## 7 - 70 minutes, 8 - 80 minutes, 9 - 90 minutes.");
+            Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine($"FAQ... hours, {Environment.NewLine} ## 1 - 10 minutes, 2 - 20 minutes, 3 - 30 minutes. {Environment.NewLine} ## 4 - 40 minutes, 5 - 50 minutes, 6 - 60 minutes. {Environment.NewLine} ## 7 - 70 minutes, 8 - 80 minutes, 9 - 90 minutes.");
             // FAQ hours, minutes.
 
             Console.WriteLine(Sharp);
 
             try
             {
-                Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine($"##### MyDistanceMove #####");
+                Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine($"##### MyDistanceMove #####");
                 Console.WriteLine("Indicate the distance you need, then indicate how fast you will travel [kilometers] : ");
                 Console.WriteLine(Sharp);
                 Console.WriteLine($"You have traveled the distance for {Math.Round(moveAndSpreed.Move(0M, 0M), 2)} hours / minutes");
@@ -26,21 +29,42 @@ namespace MyDistanceMove
             {
                 Console.WriteLine(Sharp);
                 Console.ForegroundColor = ConsoleColor.Red;  Console.WriteLine("[Error] Empty values");
+
+                using(var errorNull = new StreamWriter("errorLog.txt", true, Encoding.UTF8))
+                {
+                    errorNull.WriteLine(Log + "[Error] Empty values");
+                }
+
             }
             catch(DivideByZeroException)
             {
                 Console.WriteLine(Sharp);
                 Console.ForegroundColor = ConsoleColor.Red;  Console.WriteLine("[Error] Zero division attempt");
+
+                using (var errorNull = new StreamWriter("errorLog.txt", true, Encoding.UTF8))
+                {
+                    errorNull.WriteLine(Log + "[Error] Zero division attempt");
+                }
             }
             catch(FormatException)
             {
                 Console.WriteLine(Sharp);
                 Console.ForegroundColor = ConsoleColor.Red;  Console.WriteLine("[Error] Invalid argument specified, enter an integer");
+
+                using (var errorNull = new StreamWriter("errorLog.txt", true, Encoding.UTF8))
+                {
+                    errorNull.WriteLine(Log + "[Error] Invalid argument specified, enter an integer");
+                }
             }
             catch(OverflowException)
             {
                 Console.WriteLine(Sharp);
                 Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("[Error] Non-correct number entered");
+
+                using (var errorNull = new StreamWriter("errorLog.txt", true, Encoding.UTF8))
+                {
+                    errorNull.WriteLine(Log + "[Error] Non-correct number entered");
+                }
             }
             finally
             {
